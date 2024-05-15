b59f65488f63015d33d8117ff60b57330f70ea5f
bongoye <danielbongoye@gmail.com>
2024-05-15 14:28:03 +0300

    Update page.tsx

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -5,6 +5,7 @@ import { lusitana } from '@/app/ui/fonts';
 import { fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
 import CardWrapper from '@/app/ui/dashboard/cards';
+
 import {
   CardsSkeleton,
   LatestInvoicesSkeleton,
